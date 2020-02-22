fea341bd0d7a0f204949abdede27f3cd5302936b
octogoniaservices <61347285+octogoniaservices@users.noreply.github.com>
2020-02-22 21:37:50 +0100

    Update adblock.js

--- adblock.js
+++ adblock.js
@@ -1,27 +1,6 @@
-var REGISTER = 'https://octogoniaservices.github.io/produits';
-var REQUEST = location.href;
-function message(type,message,timeval,redirect) {
-    if(type == 1) {
-        var myclass = 'alert alert-success';
-        var title = 'OK';
-    }
-    else {
-        var myclass = 'alert-danger alert-error';
-        var title = 'ERREUR';
-    }
-    bootbox.alert('<div class="'+myclass+'" style="padding:10px;"><h2>'+title+'</h2><p>'+message+'</p></div>');
-    setTimeout(function() {
-        bootbox.hideAll();
-        if(redirect) {
-            if(redirect=='back')
-                history.back();
-            else
-                location.href = redirect;
-        }
-    }, timeval*1000);
+If (Adblock enabled) {
+   Show "Disable adblock"
 }
-$(document).ready(function() {
-    if(document.getElementById("checker") == undefined && !REQUEST.match('inscription')) {
-        message(0,"<u>Il semblerait que <b>ADBLOCK</b> soit activé sur votre navigateur</u>. Vous ne voulez plus de pub ? <a href='"+REGISTER+"'>Inscrivez-vous !</a>. Vous allez être redirigé vers la page d\'inscription dans 10 secondes... ",10,REGISTER);
-    }
-});
+else {
+   Show site
+}
