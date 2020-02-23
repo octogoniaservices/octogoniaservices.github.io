f5b0a44b15ab06f8cbc748fe1be6093d3ae4c20d
octogoniaservices <61347285+octogoniaservices@users.noreply.github.com>
2020-02-23 11:10:06 +0100

    Update ads.js

--- ads.js
+++ ads.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
         alert('Aucun bloqueur de pub a été détecté.');
         
     } else {
-        alert('Une bloqueur de pub a été détecté. Vous serez redirigez sur google.');
-        $(location).attr('href',"https://octogoniaservices.tk/minecraft");
+        alert('Une bloqueur de pub a été détecté. Vous serez rediriger sur google.');
+        $(location).attr('href',"https://google.fr");
     }
 });
