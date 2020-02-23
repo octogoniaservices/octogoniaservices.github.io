4f718a645ed918303e4ec62a561cfddcc9282919
octogoniaservices <61347285+octogoniaservices@users.noreply.github.com>
2020-02-23 13:18:54 +0100

    Update ads.js

--- ads.js
+++ ads.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
     if($("#wrapfabtest").height() > 0) {
-        console('caca')
         
     } else {
         alert('Une bloqueur de pub a été détecté. Vous serez rediriger sur une page qui vous montre comment désactiver adblock. Cliquez sur le bouton "Ok" pour continuer.');
