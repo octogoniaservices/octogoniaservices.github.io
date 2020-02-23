e13df80ab82168af1070389fd29e6d074908f747
octogoniaservices <61347285+octogoniaservices@users.noreply.github.com>
2020-02-23 14:50:49 +0100

    Update ads.js

--- ads.js
+++ ads.js
@@ -1,4 +1,29 @@
-var e=document.createElement('div');
-e.id='bpgbtbavnfreivprfgx';
-e.style.display='none';
-document.body.appendChild(e);
+$(document).ready(function(){
+    (function(){
+        var adBlockFlag = document.createElement('div');
+        adBlockFlag.innerHTML = '&nbsp;';
+        adBlockFlag.className = 'adsbox';
+        $('body').append(adBlockFlag);
+        window.setTimeout(function() {
+          if (adBlockFlag.offsetHeight === 0) {
+            showAdBlockPopUp();
+            $('body').addClass('adblock');
+          }
+          adBlockFlag.remove();
+        }, 100);
+ 
+        function showAdBlockPopUp(){
+            var adBlockerPopup = $('#adBlockerPopup');
+            adBlockerPopup.modal({
+                backdrop: 'static',
+                keyboard: false
+            });
+            adBlockerPopup.modal('show');
+        }
+ 
+        $(document).on('click', '#adBlockerPopupRefresh', function(){
+            location.reload();
+        });
+ 
+    })();
+});
