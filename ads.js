81b7b64d998506a728ebac8160a7af1571393ffb
octogoniaservices <61347285+octogoniaservices@users.noreply.github.com>
2020-02-23 11:13:44 +0100

    Update ads.js

--- ads.js
+++ ads.js
@@ -1,8 +1,5 @@
 $(document).ready(function(){
-    if($("#wrapfabtest").height() > 0) {
-        alert('Aucun bloqueur de pub a été détecté.');
-        
-    } else {
+    if($("#wrapfabtest").height() > 1) {
         alert('Une bloqueur de pub a été détecté. Vous serez rediriger sur google.');
         $(location).attr('href',"https://google.fr");
     }
