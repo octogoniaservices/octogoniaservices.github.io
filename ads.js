2621adf59acfe3a6ba56348395b4e07207198269
octogoniaservices <61347285+octogoniaservices@users.noreply.github.com>
2020-02-23 11:07:20 +0100

    Delete ads.js

--- ads.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    if($("#wrapfabtest").height() > 0) {
-        alert('No AdBlock :)');
-        
-    } else {
-        alert('AdBlock Detected ');
-        $(location).attr('href',"https://octogoniaservices.tk/minecraft");
-    }
-});
