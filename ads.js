a552705aaea11ee0fafab23aba4ca43f69f26bee
octogoniaservices <61347285+octogoniaservices@users.noreply.github.com>
2020-02-23 13:05:18 +0100

    Update ads.js

--- ads.js
+++ ads.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
     if($("#wrapfabtest").height() > 0) {
+        console('caca')
+        
+    } else {
         alert('Une bloqueur de pub a été détecté. Vous serez rediriger sur une page qui vous montre comment désactiver adblock. Cliquez sur le bouton "Ok" pour continuer.');
         $(location).attr('href',"https://www.youtube.com/watch?v=h7l1CBnrepY");
     }
