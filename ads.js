46e7a4ac8d040096b93ace0770d74f2f7ab6b7fe
octogoniaservices <61347285+octogoniaservices@users.noreply.github.com>
2020-02-23 10:56:23 +0100

    Create ads.js

---
+++ ads.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    if($("#wrapfabtest").height() > 0) {
+        alert('No AdBlock :)');
+        
+    } else {
+        alert('AdBlock Detected ');
+        
+    }
+});
